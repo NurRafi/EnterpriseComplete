259c53062a14d20d6cab7df8ca305ce1cd8feb09
Nur Rafi <NurRafi@protonmail.com>
2022-02-08 22:41:24 +0600

    Initialized Spring.

---
+++ src/main/java/com/OutputMachine/EnterpriseComplete/EnterpriseComplete.java
@@ -0,0 +1,13 @@
+package com.OutputMachine.EnterpriseComplete;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnterpriseComplete {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EnterpriseComplete.class, args);
+    }
+
+}
---
+++ src/test/java/com/OutputMachine/EnterpriseComplete/EnterpriseCompleteTests.java
@@ -0,0 +1,13 @@
+package com.OutputMachine.EnterpriseComplete;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnterpriseCompleteTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
